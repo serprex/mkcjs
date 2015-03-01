8313b395d0262f79e3b63a890e84fd75c6f70293
Demur Rumed <gunkmute@gmail.com>
2015-03-01 18:51:33 +0000

    Support json, minify plate

--- mkcjs.js
+++ mkcjs.js
@@ -1,26 +1,31 @@
 #!/usr/bin/node
-if (!process.argv.length){
+"use strict";
+if (process.argv.length<3){
 	process.stdout.write("mkcjs OUT [MODULES]\n");
 	return;
 }
-var fs = require("fs");
-var path = require("path");
+var fs = require("fs"),path = require("path");
 var out = fs.createWriteStream(process.argv[2]);
 function writeModule(n){
-	return function(){
-		if (n >= process.argv.length){
-			out.end();
-			return;
-		}
-		out.write("require.f('" + process.argv[n].slice(0,-3) + "',function(require,module,exports){")
-		var src = fs.createReadStream(process.argv[n]);
-		src.on("end", function(){
-			out.write("});")
-			writeModule(n+1)();
-		});
-		src.pipe(out, {end: false});
+	if (n >= process.argv.length){
+		out.end();
+		return;
 	}
+	var f = process.argv[n], ext = path.extname(f);
+	if (ext == ".js"){
+		out.write("require.f('" + f.slice(0,-3) + "',function(module,exports,require){");
+	}else if (ext == ".json"){
+		out.write("require.f('" + f + "',function(m){m.exports=");
+	}else{
+		console.log("Unknown ext: " + ext);
+	}
+	var src = fs.createReadStream(process.argv[n]);
+	src.on("end", function(){
+		out.write("});");
+		writeModule(n+1);
+	});
+	src.pipe(out, {end: false});
 }
 var plate = fs.createReadStream(path.resolve(__dirname, "plate.js"));
+plate.on("end", function(){writeModule(3)});
 plate.pipe(out, {end: false});
-plate.on("end", writeModule(3));
--- plate.js
+++ plate.js
@@ -1,22 +1,9 @@
-"use strict";
-var require=(function(o){
-	var M={}
-	var r=function(x,p){
-		if(x.slice(0,2)=="./")x=x.slice(2);
-		if(p)x=p+x;
-		while(x.match(/[^/]+\/\.\.\//))x=x.replace(/[^/]+\/\.\.\//,"");
-		var m=M[x];
-		return m?m():o?o(x):null;
-	}
-	r.f=function(p,f){
-		var m={exports:{}},i=true,j=p.lastIndexOf("/"),s=j==-1?"":p.slice(0,j+1);
-		M[p]=function(){
-			if (i){
-				i=false;
-				f(function(x){return r(x,s)},m,m.exports);
-			}
-			return m.exports;
-		}
-	}
-	return r;
-})(typeof require=="undefined"?null:require);
+"use strict"
+var require=(function(o){var M={},r=function(x,p){
+x=x.replace(/^\.\//,"")
+if(p)x=p+x
+while(x.match(/[^/]+\/\.\.\//))x=x.replace(/[^/]+\/\.\.\//,"")
+return x in M?M[x]():o?o(x):null}
+r.f=function(p,f){var m={exports:{}},i=true,j=p.lastIndexOf("/"),s=~j?p.slice(0,j+1):""
+M[p]=function(){if (i){i=false;f(m,m.exports,function(x){return r(x,s)})}return m.exports}}
+return r})(typeof require!="undefined"&&require);
