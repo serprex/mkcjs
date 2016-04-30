760cca9f9dd113253845a945bd8bf746282b1166
Demur Rumed <gunkmute@gmail.com>
2016-04-30 14:12:45 +0000

    Further minify plate.js

--- plate.js
+++ plate.js
@@ -1,9 +1,8 @@
 "use strict"
-var require=(function(o){var M={},r=function(x,p){
-x=x.replace(/^\.\//,"")
-if(p)x=p+x
-while(x.match(/[^/]+\/\.\.\//))x=x.replace(/[^/]+\/\.\.\//,"")
-return x in M?M[x]():o?o(x):null}
-r.f=function(p,f){var m={exports:{}},i=true,j=p.lastIndexOf("/"),s=~j?p.slice(0,j+1):""
-M[p]=function(){if (i){i=false;f(m,m.exports,function(x){return r(x,s)})}return m.exports}}
+var require=(function(o){var M={},a=/[^/]+\/\.\.\//,r=function(x,p){
+x=(p||"")+x.replace(/^\.\//,"")
+while(x.match(a))x=x.replace(a,"")
+return(M[x]||o)(x)}
+r.f=function(p,f){var m={},j=p.lastIndexOf("/"),s=~j?p.slice(0,j+1):""
+M[p]=function(){return m.exports||(f(m,m.exports={},function(x){return r(x,s)}),m.exports)}}
 return r})(typeof require!="undefined"&&require);
